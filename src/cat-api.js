8a4572cbdc2d23457a9aefed14264842a004eb2d
Nataly Martínez <nataly.97.11.m@gmail.com>
2023-06-19 21:35:19 -0500

    commit_sin_librerias_entrega_prueba

--- src/cat-api.js
+++ src/cat-api.js
@@ -23,7 +23,6 @@ export function fetchBreeds() {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
-
     }))
         .then(response => response.json())
         .then(data => {
